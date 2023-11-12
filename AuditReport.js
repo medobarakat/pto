3f4cd56b2ad01dc6d4edb096ddcd8b193b70ab97
ِAhmed Barakat <ahmedbarakat2401@gmail.com>
2023-11-12 17:38:12 +0200

    Update AuditReport.js

--- AuditReport.js
+++ AuditReport.js
@@ -12,13 +12,14 @@ import axios from "axios";
 import moment from "moment";
 // *** components ***
 import Spinner from "components/spinner";
-import { saveAs } from "file-saver";
 import Datepicker from "components/FormFields/Datepicker/formik-date-picker";
+
 const initialDate = moment(new Date()).format("MM/DD/YYYY").toString();
+
 const dateValidation = Yup.date()
   .nullable()
   .typeError("Invalid Date.")
-  .required("Required Field.");
+  .required("Required Field");
 
 const AuditReport = () => {
   const initialValues = { fromDate: initialDate, toDate: initialDate };
@@ -37,52 +38,62 @@ const AuditReport = () => {
     const a = document.createElement("a");
     a.href = url;
     a.download = "audit_report.pdf";
+
+    // Simulate a click event on the anchor to trigger the download
     a.style.display = "none";
     document.body.appendChild(a);
     a.click();
+
+    // Clean up
     document.body.removeChild(a);
     URL.revokeObjectURL(url);
   }
 
-  const onSubmit = ({ fromDate, toDate }) => {
+  const onSubmit = async ({ fromDate, toDate }) => {
+    setLoading(true);
+
     const headers = { "Content-type": "application/json" };
     const body = {
       fromDate: moment(fromDate).format("YYYY-MM-DD"),
       toDate: moment(toDate).format("YYYY-MM-DD"),
     };
 
-    axios
-      .post("/api/auditReport", body, { headers, responseType: "arraybuffer" })
-      .then((response) => {
-        console.log(response);
-        downloadPdfFromByteArray(response);
-        setLoading(false);
-      })
-      .catch((error) => {
-        setLoading(false);
-        // Handle error
+    try {
+      const response = await axios.post("/api/auditReport", body, {
+        headers,
+        responseType: "arraybuffer",
       });
+
+      downloadPdfFromByteArray(response.data);
+    } catch (error) {
+      // Handle error
+    } finally {
+      setLoading(false);
+    }
   };
 
-  // const onSubmit = ({ fromDate, toDate }) => {
+  // const onSubmit = async ({ fromDate, toDate }) => {
+  //   setLoading(true);
 
-  //   const headers = { "Content-type": "application/pdf" };
+  //   const headers = { "Content-type": "application/json" };
   //   const body = {
   //     fromDate: moment(fromDate).format("YYYY-MM-DD"),
   //     toDate: moment(toDate).format("YYYY-MM-DD"),
   //   };
 
-  //   axios
-  //     .post("http://44.211.194.102:8084/auditReport", body, { headers, responseType: "arraybuffer" })
-  //     .then((response) => {
-  //       console.log(response)
-  //       downloadPdfFromByteArray(response)
-  //       setLoading(false);
-  //     })
-  //     .catch((error) => {
-  //       setLoading(false);
-  //       // Handle error
-  //     });
+  //   try {
+  //     const response = await axios.post(
+  //       "http://44.211.194.102:8084/auditReport",
+  //       body,
+  //       { headers, responseType: "arraybuffer" }
+  //     );
+
+  //     downloadPdfFromByteArray(response.data);
+  //   } catch (error) {
+  //     // Handle error
+  //   } finally {
+  //     setLoading(false);
+  //   }
   // };
 
   return (
@@ -99,69 +110,61 @@ const AuditReport = () => {
           validationSchema={validationSchema}
           onSubmit={onSubmit}
         >
-          {(formik) => {
-            return (
-              <Form>
-                <Stack direction="row" spacing={8}>
-                  <Field name="fromDate">
-                    {({ field, form, meta }) => (
-                      <Datepicker
-                        id="fromDate"
-                        labelText="Date From"
-                        fullWidth
-                        onDateChange={() => console.log("changed")}
-                        error={Boolean(
-                          formik.touched.fromDate && formik.errors.fromDate
-                        )}
-                        helperText={
-                          formik.touched.fromDate && formik.errors.fromDate
-                            ? formik.errors.fromDate
-                            : ""
-                        }
-                        disabled={formik.isSubmitting}
-                        setFieldTouched={formik.setFieldTouched}
-                        {...formik.getFieldProps("fromDate")}
-                      />
-                    )}
-                  </Field>
-                  <Field name="toDate">
-                    {({ field, form, meta }) => (
-                      <Datepicker
-                        id="toDate"
-                        labelText="Date To"
-                        fullWidth
-                        onDateChange={() => console.log("changed")}
-                        error={Boolean(
-                          formik.touched.toDate && formik.errors.toDate
-                        )}
-                        helperText={
-                          formik.touched.toDate && formik.errors.toDate
-                            ? formik.errors.toDate
-                            : ""
-                        }
-                        disabled={formik.isSubmitting}
-                        setFieldTouched={formik.setFieldTouched}
-                        {...formik.getFieldProps("toDate")}
-                      />
-                    )}
-                  </Field>
-                </Stack>
-                <Stack
-                  sx={{ diplay: "flex", alignItems: "center", marginTop: 4 }}
-                >
-                  <Button
-                    sx={{ width: "20%" }}
-                    type="submit"
-                    variant="contained"
-                  >
-                    <span>
-                      {loading && <Spinner size={17} />}Create Audit Report
-                    </span>
-                  </Button>
-                </Stack>
-              </Form>
-            );
-          }}
+          {(formik) => (
+            <Form>
+              <Stack direction="row" spacing={8}>
+                <Field name="fromDate">
+                  {({ field, form }) => (
+                    <Datepicker
+                      id="fromDate"
+                      labelText="Date From"
+                      fullWidth
+                      onDateChange={() => console.log("changed")}
+                      error={Boolean(
+                        form.touched.fromDate && form.errors.fromDate
+                      )}
+                      helperText={
+                        form.touched.fromDate && form.errors.fromDate
+                          ? form.errors.fromDate
+                          : ""
+                      }
+                      disabled={formik.isSubmitting}
+                      setFieldTouched={formik.setFieldTouched}
+                      {...formik.getFieldProps("fromDate")}
+                    />
+                  )}
+                </Field>
+                <Field name="toDate">
+                  {({ field, form }) => (
+                    <Datepicker
+                      id="toDate"
+                      labelText="Date To"
+                      fullWidth
+                      onDateChange={() => console.log("changed")}
+                      error={Boolean(form.touched.toDate && form.errors.toDate)}
+                      helperText={
+                        form.touched.toDate && form.errors.toDate
+                          ? form.errors.toDate
+                          : ""
+                      }
+                      disabled={formik.isSubmitting}
+                      setFieldTouched={formik.setFieldTouched}
+                      {...formik.getFieldProps("toDate")}
+                    />
+                  )}
+                </Field>
+              </Stack>
+              <Stack
+                sx={{ diplay: "flex", alignItems: "center", marginTop: 4 }}
+              >
+                <Button sx={{ width: "20%" }} type="submit" variant="contained">
+                  <span>
+                    {loading ? <Spinner size={17} /> : "Create Audit Report"}
+                  </span>
+                </Button>
+              </Stack>
+            </Form>
+          )}
         </Formik>
       </CardBody>
       <CardFooter />
