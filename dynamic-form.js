0d549a88e604dad66615de25c60a2c25be2a2fd2
ِAhmed Barakat <ahmedbarakat2401@gmail.com>
2023-11-22 09:31:36 -0500

    Add files via upload

---
+++ dynamic-form.js
@@ -0,0 +1,346 @@
+import React, { memo, useMemo, useEffect, useContext } from "react";
+import _ from "lodash";
+import PropTypes from "prop-types";
+import { Formik, Form, Field } from "formik";
+import { FormControl, Grid, InputLabel, MenuItem, Stack } from "@mui/material";
+// *** components ***
+import CustomInput from "components/FormFields/CustomInput";
+import CustomButton from "components/FormFields/CustomButton";
+import IOSSwitch from "components/FormFields/CustomSwitch/IOSSwitch";
+import CustomRadioGroup from "components/FormFields/CustomRadioGroup";
+import Datepicker from "components/FormFields/Datepicker/formik-date-picker";
+// *** modals ***
+import PromptLeavingDialog from "shared/modals/accept-reject.dialog";
+// *** context ***
+import { AnalysisNavigatingAwayContext } from "components/views/analysis/tabAnalysis";
+import { ExceptionsNavigatingAwayContext } from "views/exceptions";
+import { AddExceptionNavigatingAwayContext } from "views/add-exception";
+import { Select } from "formik-mui";
+import CustomDropdown from "components/FormFields/CustomDropdown";
+
+const DynamicForm = React.forwardRef(
+  (
+    {
+      viewType,
+      formId,
+      valueTypes,
+      hardReload,
+      initialValues,
+      validationSchema,
+      onSubmit,
+      formFields,
+    },
+    ref
+  ) => {
+    const SelectedNavAwayContext = useMemo(() => {
+      switch (viewType) {
+        case "exception":
+          return ExceptionsNavigatingAwayContext;
+        case "add-exception":
+          return AddExceptionNavigatingAwayContext;
+        default:
+          return AnalysisNavigatingAwayContext;
+      }
+    }, [viewType]);
+
+    const {
+      canShowDialogLeavingPage,
+      showDialogLeavingPage,
+      cancelNavigation,
+      confirmNavigation,
+      setCanShowDialogLeavingPage,
+      setHardReload,
+    } = useContext(SelectedNavAwayContext);
+
+    useEffect(() => {
+      if (hardReload) {
+        setCanShowDialogLeavingPage(false);
+        if (viewType === "exception" || viewType === "add-exception") {
+          setHardReload(true);
+        }
+        setTimeout(() => {
+          window.location.reload();
+        }, 200);
+      }
+    }, [hardReload]);
+
+    const onReset = () =>
+      new Promise(function (resolve, reject) {
+        // eslint-disable-next-line no-restricted-globals
+        const userChoice = confirm(
+          `would you like to reset form (${formId}) ?`
+        );
+        if (userChoice === true) {
+          setCanShowDialogLeavingPage(false);
+          resolve();
+        } else reject();
+      });
+
+    const handleFormChange = (dirty, event) => {
+      if (dirty && !canShowDialogLeavingPage) setCanShowDialogLeavingPage(true);
+    };
+
+    return (
+      <Formik
+        innerRef={ref}
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={(values) => onSubmit(values, { formId, valueTypes })}
+        onReset={onReset}
+      >
+        {(formik) => {
+          return (
+            <>
+              <PromptLeavingDialog
+                title="Do you want to leave this page?"
+                description="Changes you made may not be saved."
+                confirmButtonTitle="Leave"
+                cancelButtonTitle="Stay"
+                open={showDialogLeavingPage}
+                onClose={() => setCanShowDialogLeavingPage(false)}
+                onAccept={confirmNavigation}
+                onReject={cancelNavigation}
+                fullWidth
+              />
+
+              <Form
+                //! CAUTION:
+                //! please keep dirty (true) here onChange Form. We will not take (formik.dirty) because it returns (false) on first change
+                //! by default but our plan is to make it dirty on any change even if we have one change!!!
+                onChange={(event) => handleFormChange(true, event)}
+                onSubmit={formik.handleSubmit}
+              >
+                <Grid container columnSpacing={3} rowSpacing={1}>
+                  {formFields.map(
+                    (
+                      { id, name, type, value, options, required, choices },
+                      Idx
+                    ) => {
+                      if (choices && type === "text") {
+                        return (
+                          <Grid key={`grid-${name}`} item md={6}>
+                            <Field name={name}>
+                              {({ field, form, meta }) => {
+                                return (
+                                  <CustomDropdown
+                                    id={id}
+                                    label={name}
+                                    items={choices}
+                                    dynamicItem={true}
+                                    {...field}
+                                    name={name}
+                                    // value={field.value}
+                                    onChange={field.onChange}
+                                    />
+                                );
+                              }}
+                            </Field>
+                          </Grid>
+                        );
+                      } else {
+                        switch (type) {
+                          case "readonly":
+                          case "readonly-text":
+                          case "text":
+                          case "multi-line-text":
+                            const otherTextfieldProps = {};
+
+                            if (
+                              (type === "readonly" ||
+                                type === "readonly-text") &&
+                              value === ""
+                            ) {
+                              _.assign(otherTextfieldProps, { shrink: false });
+                            }
+
+                            if (type === "multi-line-text") {
+                              _.assign(otherTextfieldProps, {
+                                multiline: true,
+                                minRows: 4,
+                                maxRows: 6,
+                              });
+                            }
+
+                            return (
+                              <Grid key={`grid-${name}`} item md={6}>
+                                <Field name={id}>
+                                  {({
+                                    field,
+                                    form,
+                                    meta: { touched, error },
+                                  }) => (
+                                    <CustomInput
+                                      key={`form-textField-${id}`}
+                                      labelText={name}
+                                      error={Boolean(touched && error)}
+                                      helperText={
+                                        Boolean(touched && error) ? error : ""
+                                      }
+                                      // error={Boolean(formik.touched?.[id] && formik.errors?.[id])}
+                                      // helperText={formik.touched?.[id] && formik.errors?.[id] ? formik.errors?.[id] : ""}
+                                      required={required}
+                                      fullWidth
+                                      margin="normal"
+                                      disabled={formik.isSubmitting}
+                                      readOnly={Boolean(
+                                        type === "readonly" ||
+                                          type === "readonly-text"
+                                      )}
+                                      {...field}
+                                      {...otherTextfieldProps}
+                                    />
+                                  )}
+                                </Field>
+                              </Grid>
+                            );
+                          case "radio-buttons":
+                            return (
+                              <Grid key={`grid-${name}`} item md={6}>
+                                <Field name={id}>
+                                  {({ field, form, meta }) => {
+                                    return (
+                                      <CustomRadioGroup
+                                        name={id}
+                                        value={field.value}
+                                        formLabel={name}
+                                        direction="row"
+                                        size="small"
+                                        labelPlacement="end"
+                                        margin="normal"
+                                        required={required}
+                                        fullWidth
+                                        defaultValue={value}
+                                        disabled={formik.isSubmitting}
+                                        onChange={field.onChange}
+                                        options={options.map(
+                                          ({ id, name }) => ({
+                                            id,
+                                            label: name,
+                                            value: id,
+                                          })
+                                        )}
+                                        error={Boolean(
+                                          meta.touched && meta.error
+                                        )}
+                                        helperText={
+                                          Boolean(meta.touched && meta.error)
+                                            ? meta.error
+                                            : ""
+                                        }
+                                      />
+                                    );
+                                  }}
+                                </Field>
+                              </Grid>
+                            );
+                          case "boolean":
+                            return (
+                              <Grid key={`grid-${name}`} item md={6}>
+                                <Field name={id}>
+                                  {({ field, form, meta }) => (
+                                    <IOSSwitch
+                                      key={`form-switch-${id}`}
+                                      name={id}
+                                      label={name}
+                                      checked={Boolean(field.value)}
+                                      disabled={formik.isSubmitting}
+                                      required={required}
+                                      error={Boolean(
+                                        formik.touched?.[id] &&
+                                          formik.errors?.[id]
+                                      )}
+                                      helperText={
+                                        formik.touched?.[id] &&
+                                        formik.errors?.[id]
+                                          ? formik.errors?.[id]
+                                          : ""
+                                      }
+                                      onChange={field.onChange}
+                                    />
+                                  )}
+                                </Field>
+                              </Grid>
+                            );
+                          case "date":
+                            return (
+                              <Grid key={`grid-${name}`} item md={6}>
+                                <Field name={id}>
+                                  {({ field, form, meta }) => {
+                                    return (
+                                      <div
+                                        style={{
+                                          marginTop: 15,
+                                          marginBottom: 8,
+                                        }}
+                                      >
+                                        <Datepicker
+                                          id={id}
+                                          labelText={name}
+                                          fullWidth
+                                          dynamicForm
+                                          error={Boolean(
+                                            formik.touched?.[id] &&
+                                              formik.errors?.[id]
+                                          )}
+                                          helperText={
+                                            formik.touched?.[id] &&
+                                            formik.errors?.[id]
+                                              ? formik.errors?.[id]
+                                              : ""
+                                          }
+                                          disabled={formik.isSubmitting}
+                                          setFieldTouched={
+                                            formik.setFieldTouched
+                                          }
+                                          handleFormChange={handleFormChange}
+                                          {...field}
+                                        />
+                                      </div>
+                                    );
+                                  }}
+                                </Field>
+                              </Grid>
+                            );
+                          default:
+                            break;
+                        }
+                      }
+                    }
+                  )}
+                </Grid>
+
+                <Stack sx={{ mt: 2 }} direction="row" spacing={3}>
+                  <CustomButton
+                    type="submit"
+                    innerContent={
+                      formik.isSubmitting ? "Please Wait..." : "Save"
+                    }
+                    variant="contained"
+                    color="primary"
+                    disabled={formik.isSubmitting}
+                  />
+
+                  <CustomButton
+                    type="reset"
+                    innerContent="Reset"
+                    variant="outlined"
+                    color="primary"
+                    disabled={formik.isSubmitting}
+                  />
+                </Stack>
+              </Form>
+            </>
+          );
+        }}
+      </Formik>
+    );
+  }
+);
+
+DynamicForm.propTypes = {
+  initialValues: PropTypes.object.isRequired,
+  validationSchema: PropTypes.object,
+  onSubmit: PropTypes.func.isRequired,
+};
+
+export default memo(DynamicForm);
---
+++ index.js
@@ -0,0 +1,103 @@
+import React from "react";
+import propTypes from "prop-types";
+import classNames from "classnames";
+import { FormControl, Select, InputLabel, MenuItem } from "@mui/material";
+//*** styles ***
+import { useTheme } from "react-jss";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { createUseStyles } from "react-jss";
+import {
+  CustomDropdownStyles,
+  ThemeOverrides,
+} from "assets/styles/components/FormFields/customDropdown.styles";
+const useStyles = createUseStyles(CustomDropdownStyles);
+
+function CustomDropdown({
+  id,
+  variant,
+  label,
+  items,
+  value,
+  maxWidth,
+  onChange,
+  dynamicItem,
+}) {
+  const systemTheme = useTheme();
+  const theme = createTheme(ThemeOverrides(systemTheme));
+  const classes = useStyles();
+
+  return (
+    <FormControl className={classes.formControl} fullWidth>
+      <ThemeProvider theme={theme}>
+        {label && <InputLabel id={`${id}--label`}>{label}</InputLabel>}
+        <Select
+          id={id}
+          variant={variant}
+          label={label && label}
+          value={value}
+          onChange={onChange}
+          className={classNames(classes.select, {
+            [classes.standardSelect]: variant === "standard",
+          })}
+          MenuProps={{
+            sx: { maxWidth: maxWidth },
+          }}
+        >
+          {dynamicItem
+            ? items.map((item) => (
+                <MenuItem
+                  key={item}
+                  className={classes.selectMenuItem}
+                  value={item}
+                  title={item}
+                >
+                  <div
+                    style={{
+                      maxWidth: maxWidth,
+                      textOverflow: "ellipsis",
+                      overflow: "hidden",
+                    }}
+                  >
+                    {item}
+                  </div>
+                </MenuItem>
+              ))
+            : items.map((item) => (
+                <MenuItem
+                  key={item.id}
+                  id={item.id}
+                  className={classes.selectMenuItem}
+                  value={item.value}
+                  title={item.text}
+                >
+                  <div
+                    style={{
+                      maxWidth: maxWidth,
+                      textOverflow: "ellipsis",
+                      overflow: "hidden",
+                    }}
+                  >
+                    {item.text}
+                  </div>
+                </MenuItem>
+              ))}
+        </Select>
+      </ThemeProvider>
+    </FormControl>
+  );
+}
+
+CustomDropdown.defaultProps = {
+  variant: "standard",
+  maxWidth: "-webkit-fill-available",
+};
+
+CustomDropdown.propTypes = {
+  id: propTypes.oneOfType([propTypes.string, propTypes.number]),
+  variant: propTypes.oneOf(["filled", "outlined", "standard"]),
+  items: propTypes.arrayOf(propTypes.object).isRequired,
+  value: propTypes.string,
+  onChange: propTypes.func,
+};
+
+export default React.memo(CustomDropdown);
